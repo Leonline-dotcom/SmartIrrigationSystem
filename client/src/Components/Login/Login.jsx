bfd5548657cbb6f0b7fd4c1facede7a526f9dd41
Leonline-dotcom <shajijleon@gmail.com>
2023-12-28 19:15:49 -0600

    Finished the Login page 1.0

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
         </div>
 
         <form action="" className='form grid'>
-          <span>Login Status goes here</span>
+          <span className='showMessage'>Login Status goes here</span>
 
           <div className="inputDiv">
             <label htmlFor="username">Username</label>
