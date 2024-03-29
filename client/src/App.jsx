70bf13f4b662c53316425cfdf7249ce8b7929b1a
Leonline-dotcom <shajijleon@gmail.com>
2024-03-28 22:05:12 -0500

    Installed yarn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react';
 import './App.scss'
 import Login from './Components/Login/Login'
 import Register from './Components/Register/Register'
-import Setup from './Components/Setup/ConnectionStatus'
-import ESP32 from './Components/Examples/ESP32Example'
-import Template from './Components/Examples/Templates'
-import Zones from './Components/Zones/ZoneList'
-import Zone from './Components/Zones/Zone'
+// import Setup from './Components/Setup/ConnectionStatus'
+// import ESP32 from './Components/Examples/ESP32Example'
+// import Template from './Components/Examples/Templates'
+// import Zones from './Components/Zones/ZoneList'
+import ZoneOverview from './Components/Zones/ZoneOverview'
 import WaterCap from './Components/TankCapacity/WaterCap'
 import Weather from  './Components/Weather Display/Weather'
-import Interface from './Components/Examples/Interface';
+// import Interface from './Components/Examples/Interface';
 import {createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 function App(){
@@ -28,10 +28,10 @@ const router = createBrowserRouter([
     path: '/zones',
     element: <ZoneOverview/>,
   },
-  {
-    path: '/interface',
-    element: <Interface/>
-  },
+  // {
+  //   path: '/interface',
+  //   element: <Interface/>
+  // },
   {
     path: '/capacity',
     element: <WaterCap/>
