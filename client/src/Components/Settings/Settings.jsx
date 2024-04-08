84d3869ea7145cad4d46b4379195274df7e6ca5d
Kingaus <austinking@utexas.edu>
2024-04-08 17:49:26 -0500

    SSE connection status and voltage

--- client/src/Components/Settings/Settings.jsx
+++ client/src/Components/Settings/Settings.jsx
@@ -10,36 +10,52 @@ function Settings(){
       });
 
     //Comment out which one the test enviroment is not.
-    // const API_URL = "http://10.159.64.194:5001";  //Local Host URL
-    const API_URL = "http://oasis-flow.com";      //Website URL
+    const API_URL = "http://10.159.66.199:5001";  //Local Host URL
+    // const API_URL = "http://oasis-flow.com";      //Website URL
 
 
 
     useEffect(() => {
-        const esp32StatusSource = new EventSource(`${API_URL}/api/esp32-status-stream`);
-        esp32StatusSource.onmessage = (event) => {
-          const status = JSON.parse(event.data);
-          setEsp32Status(status);
+        // Function to initiate the SSE connection and handle incoming messages.
+        const initiateSSE = (url, onMessage) => {
+            const source = new EventSource(url);
+
+            source.onmessage = (event) => {
+                const data = JSON.parse(event.data);
+                onMessage(data);
+            };
+
+            source.onerror = (error) => {
+                console.error("SSE error:", error);
+                source.close(); // Close the existing connection
+
+                // Attempt to reconnect after a timeout
+                setTimeout(() => initiateSSE(url, onMessage), 5000);
+            };
+
+            return source;
         };
 
-        const batteryLevelSource = new EventSource(`${API_URL}/api/battery-level-stream`);
-        batteryLevelSource.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        setSystemStatus(prevState => ({
-            ...prevState,
-            battery: `${data.voltage} V` // Format as needed
-        }));
-        };
+        // Establishing SSE connections for ESP32 status and battery level
+        const esp32StatusSource = initiateSSE(`${API_URL}/api/esp32-status-stream`, (data) => {
+            setEsp32Status(data);
+        });
 
+        const batteryLevelSource = initiateSSE(`${API_URL}/api/battery-level-stream`, (data) => {
+            setSystemStatus(prevState => ({
+                ...prevState,
+                battery: `${data.voltage} V` // Format as needed
+            }));
+        });
 
+        // Cleanup function to close the SSE connections when the component unmounts or re-renders
         return () => {
             esp32StatusSource.close();
             batteryLevelSource.close();
         };
 
+    }, [API_URL]); // Dependency array to re-run the effect if API_URL changes
 
-
-    }, []);
     return(
         <div className="settings">
         <h1>Smart Irrigation Settings</h1>
