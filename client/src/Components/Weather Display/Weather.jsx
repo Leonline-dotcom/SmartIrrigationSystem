e2b4f69b75eb53afd200efbe2272530572d1a04b
Leonline-dotcom <shajijleon@gmail.com>
2024-04-09 16:03:56 -0500

    Made changes to weather.jsx

--- client/src/Components/Weather Display/Weather.jsx
+++ client/src/Components/Weather Display/Weather.jsx
@@ -1,8 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import './Weather.css';
-
-const url ="http://10.154.162.126:5001";
+const url ="http://10.145.229.146:5001";
 
 const Weather = () => {
   const [weatherData, setWeatherData] = useState(null);
@@ -30,14 +29,10 @@ const Weather = () => {
             <p>{weatherData.name}</p>
           </div>
           <div className='temp'>
-            <h1>{Math.round(weatherData.main.temp *(9/5) + 32)}°F</h1>
+            <h1>{Math.round(weatherData.main.temp)}°F</h1>
           </div>
           <div className='description'>
             <p>{weatherData.weather[0].description}</p>
-          </div>
-          <div className='icon'>
-             
-          <img alt="icon" src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} width="120" height="100" />
           </div>
         </>
       )}
